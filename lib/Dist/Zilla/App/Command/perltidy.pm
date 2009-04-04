65fc5f0f71f7303694fd4a95431a68f2806b7fd6
Fayland 林 <fayland@gmail.com>
2009-04-04 14:08:47 +0000

    Import of FAYLAND/Dist-Zilla-Plugin-Perltidy-0.01 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-PerlTidy
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FAYLAND/Dist-Zilla-Plugin-Perltidy-0.01.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/App/Command/perltidy.pm
@@ -0,0 +1,69 @@
+package Dist::Zilla::App::Command::perltidy;
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+# ABSTRACT: perltidy your dist
+use Dist::Zilla::App -command;
+
+sub abstract { 'perltidy your dist' }
+
+sub run {
+    my ( $self, $opt, $arg ) = @_;
+
+    my $perltidyrc;
+    if ( scalar @$arg and -e $arg->[0] ) {
+        $perltidyrc = $arg->[0];
+    }
+
+# XXX? TODO
+#    my $config = $self->config;
+#    $perltidyrc = ( exists $config->{perltidyrc} and -e $config->{perltidyrc} ) ?
+#        $config->{perltidyrc} : undef;
+
+    # make Perl::Tidy happy
+    local @ARGV = ();
+
+    require Perl::Tidy;
+    require File::Copy;
+    require File::Next;
+
+    my $files = File::Next::files('.');
+    while ( defined( my $file = $files->() ) ) {
+        next unless ( $file =~ /\.(t|p[ml])$/ );    # perl file
+        my $tidyfile = $file . '.tdy';
+        Perl::Tidy::perltidy(
+            source      => $file,
+            destination => $tidyfile,
+            perltidyrc  => $perltidyrc,
+        );
+        File::Copy::move( $tidyfile, $file );
+    }
+
+    return 1;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Dist::Zilla::App::Command::perltidy - perltidy a dist
+
+=head1 SYNOPSIS
+
+    $ dzil perltidy
+    # OR
+    $ dzil perltidy .myperltidyrc
+
+=head1 AUTHOR
+
+Fayland Lam, C<< E<lt>fayland@gmail.comE<gt> >>
+
+=head1 COPYRIGHT
+
+Copyright 2008, Fayland Lam.
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
---
+++ lib/Dist/Zilla/Plugin/PerlTidy.pm
@@ -0,0 +1,72 @@
+package Dist::Zilla::Plugin::PerlTidy;
+our $VERSION = '0.01';
+
+# ABSTRACT: PerlTidy in Dist::Zilla
+
+use Moose;
+with 'Dist::Zilla::Role::FileMunger';
+
+has '_perltidyrc';
+
+sub munge_file {
+    my ( $self, $file ) = @_;
+
+    return $self->munge_perl($file) if $file->name    =~ /\.(?:pm|pl|t)$/i;
+    return $self->munge_perl($file) if $file->content =~ /^#!perl(?:$|\s)/;
+    return;
+}
+
+sub munge_perl {
+    my ( $self, $file ) = @_;
+
+    my $content = $file->content;
+
+    my $perltidyrc;
+
+    # XXX? TODO
+    # = ( $self->perltidyrc and exists $self->perltidyrc ) ?
+    #    $self->perltidyrc : undef;
+
+    # make Perl::Tidy happy
+    local @ARGV = ();
+
+    my $tided;
+    require Perl::Tidy;
+    Perl::Tidy::perltidy(
+        source      => \$content,
+        destination => \$tided,
+        perltidyrc  => $perltidyrc,
+    );
+
+    $file->content($tided);
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+__END__
+
+=head1 NAME
+
+Dist::Zilla::Plugin::PerlTidy - PerlTidy in Dist::Zilla
+
+=head1 SYNOPSIS
+
+    # dist.ini
+    [PerlTidy]
+
+=head1 TODO
+
+Make perltidyrc configurable
+
+=head1 AUTHOR
+
+Fayland Lam, C<< E<lt>fayland@gmail.comE<gt> >>
+
+=head1 COPYRIGHT
+
+Copyright 2008, Fayland Lam.
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
